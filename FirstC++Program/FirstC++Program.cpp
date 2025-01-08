a766241993430ffe1c93e15933b8ceae87d5abc3
booyaalex <isidororossinia@gmail.com>
2025-01-08 10:42:15 -0500

    Various Changes Finished Driving Game Polished the Console Output Added Functionality to Quit the Number Game

--- FirstC++Program/FirstC++Program.cpp
+++ FirstC++Program/FirstC++Program.cpp
@@ -14,57 +14,79 @@ static int generateRandomNumber(int min, int max) {
 	int random_number = distrib(gen);
 	return random_number;
 }
+static string toUpperCase(string str) {
+	string output = "";
+	for (char& c : str) {
+		output += toupper(c);
+	}
+	return output;
+}
 
 void playNumberGuessingGame() {
-	string user_play_again_input;
-	int user_input;
-	int answer = generateRandomNumber(1, 50);
+	string user_input;
+	int user_answer;
+	int correct_answer = generateRandomNumber(1, 50);
 	int number_of_guesses = 0;
 	bool game_finished = false;
 
-	cout << "\n --Number Guessing Game-- \n";
+	system("cls");
+	cout << "--- Number Guessing Game ---\n";
 
 	while (!game_finished) {
 		number_of_guesses++;
 		cout << "Guess a number between 1 and 50: \n";
 
 		cin >> user_input;
-		if (user_input > 50) {
-			cerr << "Invalid Input \n";
-			number_of_guesses--;
-		}
-		else if (user_input == answer) {
-			cout << "Correct! The answer was " << answer << "! \n";
-			cout << "It took you " << number_of_guesses << " tries to guess it! \n";
-			cout << "Type \"y\" to play again \n";
-
-			cin >> user_play_again_input;
-			if (user_play_again_input == "y") {
-				answer = generateRandomNumber(1, 50);
-				number_of_guesses = 0;
-				continue;
-			}
-			else {
-				game_finished = true;
-				break;
-			}
-		}
-		else if (user_input > answer) {
-			cout << "Wrong, the correct answer is smaller. \n";
-		}
-		else if (user_input < answer) {
-			cout << "Wrong, the correct answer is bigger. \n";
+		if (toUpperCase(user_input) == "QUIT") {
+			game_finished = true;
+			system("cls");
+			break;
 		}
 		else {
-			cerr << "Invalid Input \n";
-			number_of_guesses--;
+			try {
+				user_answer = stoi(user_input);
+				if (user_answer > 50) {
+					cerr << "Invalid Input \n";
+					number_of_guesses--;
+				}
+				else if (user_answer == correct_answer) {
+					cout << "Correct! The answer was " << correct_answer << "! \n";
+					cout << "It took you " << number_of_guesses << " tries to guess it! \n";
+					cout << "Type \"y\" to play again \n";
+
+					cin >> user_input;
+					if (user_input == "y") {
+						playNumberGuessingGame();
+						break;
+					}
+					else {
+						game_finished = true;
+						system("cls");
+						break;
+					}
+				}
+				else if (user_answer > correct_answer) {
+					cout << "Wrong, the correct answer is smaller. \n";
+				}
+				else if (user_answer < correct_answer) {
+					cout << "Wrong, the correct answer is bigger. \n";
+				}
+				else {
+					cerr << "Invalid Input \n";
+					number_of_guesses--;
+				}
+			}
+			catch (invalid_argument& e) {
+				cerr << "Invalid Input \n";
+				number_of_guesses--;
+			}
 		}
+		
+		
 	}
-	cout << "\n";
 }
 
 class DrivingGame {
-	
 	public:
 		class Car {
 			int xPos = 15;
@@ -80,6 +102,8 @@ class DrivingGame {
 				}
 		};
 		class Display {
+			int GAME_WIDTH = 41;
+			int GAME_HEIGHT = 29;
 			deque<int> gameScreen;
 			enum RoadDirection {
 				LEFT,
@@ -89,7 +113,7 @@ class DrivingGame {
 			enum RoadDirection mode = STRAIGHT;
 			public:
 				Display() {
-					for (int i = 0; i < 30; i++) {
+					for (int i = 0; i < GAME_HEIGHT; i++) {
 						gameScreen.push_back(10);
 					}
 				}
@@ -98,14 +122,20 @@ class DrivingGame {
 					int row;
 					int xPos = c.getXPos();
 
-					for (int i = 0; i < gameScreen.size(); i++) {
+					for (int i = 0; i < GAME_HEIGHT; i++) {
 						row = gameScreen[i];
 						display_string = string(row, '.') + string(21, ' ') + string(20 - row, '.');
-						if (i == gameScreen.size() - 4) {
+						if (i == GAME_HEIGHT - 4) {
 							display_string.replace(xPos, 1, 1, '^');
 						}
 						cout << display_string << "\n";
 					}
+				}
+				bool checkIfHit(DrivingGame::Car c) {
+					int next_row = gameScreen.at(GAME_HEIGHT - 5);
+					int xPos = c.getXPos();
+
+					return xPos < next_row || xPos > next_row + 20;
 				}
 				void displayGame(DrivingGame::Car c) {
 					string display_string;
@@ -160,10 +190,10 @@ class DrivingGame {
 					}
 					
 					system("cls");
-					for (int i = 0; i < gameScreen.size(); i++) {
+					for (int i = 0; i < GAME_HEIGHT; i++) {
 						row = gameScreen.at(i);
 						display_string = string(row, '.') + string(21, ' ') + string(20 - row, '.');
-						if (i == gameScreen.size() - 4) {
+						if (i == GAME_HEIGHT - 4) {
 							display_string.replace(xPos, 1, 1, '^');
 						}
 						cout << display_string << "\n";
@@ -173,9 +203,22 @@ class DrivingGame {
 };
 
 void playDrivingGame() {
+	string user_input;
 	DrivingGame::Car car;
 	DrivingGame::Display display;
 	bool game_finished = false;
+
+	system("cls");
+	cout << "--- Forza Horizon -1 ---\n";
+	cout << "Get Ready \n";
+	Sleep(1000);
+	cout << "3\n";
+	Sleep(1000);
+	cout << "2\n";
+	Sleep(1000);
+	cout << "1\n";
+	Sleep(1000);
+
 	
 	display.setUpScreen(car);
 	while (!game_finished) {
@@ -185,7 +228,23 @@ void playDrivingGame() {
 		else if (GetKeyState(VK_RIGHT) & 0x8000) {
 			car.moveRight();
 		}
+
 		display.displayGame(car);
+
+		if (display.checkIfHit(car)) {
+			cout << "Game Over! Type \"y\" to Play Again! \n";
+
+			cin >> user_input;
+			if (user_input == "y") {
+				playDrivingGame();
+				break;
+			}
+			else {
+				game_finished = true;
+				system("cls");
+				break;
+			}
+		}
 		Sleep(20);
 	}
 }
@@ -193,14 +252,15 @@ void playDrivingGame() {
 int main() {
 	string user_input;
 	bool program_done = false;
-	cout << "Welcome to the CPP Console App! \n\n";
 
 	while (!program_done) {
+		cout << "Welcome to the CPP Console App! \n\n";
 		cout << "Type in a Number to Select a Program or \"quit\" to Exit the Console \n";
 		cout << "(1) Guess the Number \n";
 		cout << "(2) Forza Horizon -1 \n";
+		cout << "(3) Alphbetically Sort Words \n";
 
-		getline(cin, user_input);
+		cin >> user_input;
 		if (user_input == "quit") { 
 			program_done = true;
 			break; 
@@ -211,11 +271,14 @@ int main() {
 		else if (user_input == "2") {
 			playDrivingGame();
 		}
+		else if (user_input == "3") {
+			playDrivingGame();
+		}
 		else {
 			cerr << "Invalid Input";
 		}
 	}
-	cout << "Thank you for using the Console App!";
+	cout << "\nThank you for Using the Console App!";
 	return 0;
 }
 
